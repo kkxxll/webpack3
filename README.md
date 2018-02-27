b6d150fb628f791f5ae70673538f4a1e804913e7
kkxxll <550893685@qq.com>
2018-02-27 21:45:43 +0800

    编译es6

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
-添加 webpack.config.js
-执行一下命令即可完成打包
-> webpack
+npm init
 
-由于文件名添加hash，所以index.html需要修改
+
+npm i -D babel-loader babel-core
+
+##### 安装最新版
+npm i babel-loader@8.0.0-beta.0 @babel/core
+
+配置好babel-loader以后
+需要通过 babel presets 声明以哪一种规范编译
+
+###### 如以下几种（还有其他的）
+- es2015
+- es2016
+- es2017
+- env（包含前三个）
+- babel-preset-react
+
+npm i @babel/preset-env -D
+npm i babel-preset-env -D
+
+targets: 用来指定对哪些语法进行编译
+
+执行webpack后，可以看到es6编译后的结果
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 module.exports = {
     entry: {
-        bundle: './index.js'
+        bundle: './app.js'
     },
     output: {
-        // filename: '[name].[hash:5].js'
-        filename: '[name].js'
+        filename: '[name].[hash:8].js'
+        // filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', {
+                                targets: {
+                                    browsers: ['> 1%', 'last 2 versions']
+                                }
+                            }]
+                        ]
+                    }
+                },
+                exclude: '/node_modules/'
+            }
+        ]
     }
 }
